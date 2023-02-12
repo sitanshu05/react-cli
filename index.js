39072c35c0e93d0d53a6a54dc523af1e617b35b6
Sitanshu Hallad <sitanshu@Sitanshus-MacBook-Pro.local>
2023-02-12 11:01:28 +0530

    minor bug fix

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ async function parser() {
 	// else if (input[0] == 'router') {
 	// 	if (input[1] == 'init') {
 	// 		pages.routingInit();
-	// 	}
+	}
 	}
 	else if(input[0]=='rename'){
 		if(input[1]=='hook' || input[1] == "h"){
